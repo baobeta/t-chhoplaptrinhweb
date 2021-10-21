0e2cff93aa2402667dc67301649344e3c6a348a1
lion160801 <82790079+lion160801@users.noreply.github.com>
2021-10-21 08:34:28 +0700

    persistence

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+<
+techshop.iml,a\8\a89d7197650fd3bcaf9b0ac5d3e72aa7278bc570
--- src/main/java/com/example/techshop/HelloServlet.java
+++ src/main/java/com/example/techshop/HelloServlet.java
@@ -1,27 +1,16 @@
 package com.example.techshop;
 
+import com.example.techshop.entity.RoleEntity;
 import java.io.*;
+import javax.servlet.ServletException;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
-@WebServlet(name = "helloServlet", value = "/hello-servlet")
+@WebServlet("/test")
 public class HelloServlet extends HttpServlet {
-    private String message;
 
-    public void init() {
-        message = "Hello World!";
-    }
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
-
-        // Hello
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>");
-    }
-
-    public void destroy() {
-    }
 }
